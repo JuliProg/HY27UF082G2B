262606befc9fae8937031499569175da227f3219
JuliProg <60358225+JuliProg@users.noreply.github.com>
2021-11-12 19:13:14 +0200

    Update ChipAssembly.cs
    Update chip parameters

--- HY27UF082G2B/ChipAssembly.cs
+++ HY27UF082G2B/ChipAssembly.cs
@@ -42,13 +42,13 @@ namespace HY27UF082G2B
             myChip.chipID = "ADDA109544";            // device ID - ADh 73h (HY27UF082G2B.pdf page 30)
 
             myChip.width = Organization.x8;    // chip width - 8 bit
-            myChip.bytesPP = 2048;             // page size - 512 byte
-            myChip.spareBytesPP = 64;          // size Spare Area - 16 byte
-            myChip.pagesPB = 64;               // the number of pages per block - 32 
-            myChip.bloksPLUN = 2048;           // number of blocks in CE - 1024
+            myChip.bytesPP = 2048;             // page size - 2048 byte
+            myChip.spareBytesPP = 64;          // size Spare Area - 64 byte
+            myChip.pagesPB = 64;               // the number of pages per block - 64 
+            myChip.bloksPLUN = 2048;           // number of blocks in CE - 2048
             myChip.LUNs = 1;                   // the amount of CE in the chip
-            myChip.colAdrCycles = 1;           // cycles for column addressing
-            myChip.rowAdrCycles = 2;           // cycles for row addressing 
+            myChip.colAdrCycles = 2;           // cycles for column addressing
+            myChip.rowAdrCycles = 3;           // cycles for row addressing 
             myChip.vcc = Vcc.v3_3;             // supply voltage
             (myChip as ChipPrototype_v1).EccBits = 1;                // required Ecc bits for each 512 bytes
 
